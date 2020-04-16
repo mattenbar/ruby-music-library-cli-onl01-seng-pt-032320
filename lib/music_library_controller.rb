ceb97162aa0af79331a7a2a0a7237a102bb175d9
matthew enbar <matthew.enbar@gmail.com>
2020-04-16 18:43:32 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,8 +37,8 @@
   end
 
   def list_songs
-    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
-      puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index do |s, i|
+      puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
   end
 

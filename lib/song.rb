0143c0b7c579f512a8459bac8119ae93a3e03816
matthew enbar <matthew.enbar@gmail.com>
2020-04-16 18:49:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,8 @@ class Song
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist=(artist) if artist != nil
-    self.genre=(genre) if genre != nil
+    self.artist=(artist) if artist
+    self.genre=(genre) if genre
     save
   end
 

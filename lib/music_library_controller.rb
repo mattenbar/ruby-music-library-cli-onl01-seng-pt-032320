55c5030f2da443f5c3a4408b8325b6cce8cf4295
matthew enbar <matthew.enbar@gmail.com>
2020-04-16 19:42:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -71,6 +71,9 @@ end
     puts "Which song number would you like to play?"
 
     input = gets.strip.to_i
+    if input > 1 && input < Song.all.length
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+    end
 
   end
 
